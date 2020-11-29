c71a9ecbceb8587673fd1206bda8467377d5624f
ken-excel <isolate.xcl@gmail.com>
2020-11-30 02:15:57 +0900

    en_text_v1

--- app/ken-portfolio/src/text/en.js
+++ app/ken-portfolio/src/text/en.js
@@ -252,7 +252,7 @@ const showcase = {
                 {
                     name: "Autonomous Driving Robot from Scratch",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: " ",
+                    summary: "With a 3 person team, we developed an autonomous driving robot from scratch. The robot has following setup: Jetson TX2 as a computing unit and Velodyne VLP-16 3D LIDAR for mapping and odometry.",
                     paragraphs:["I built an autonomous driving robot from scratch as a part time job along with 2 other students. We split the roles into three: software, mechanical, and electrical. I took the software role in this project",
                     "While the robot is still under development and requires improvement in performance, The robot is able to move around, make a map, autonomous navigates while carrying a small cargo.",
                     "The robot base is differential drive type, equipped with a 3D Laser Rangefinder Velodyne VLP-16 and Nvidia Jetson TX2 board as a computing unit",
@@ -267,6 +267,10 @@ const showcase = {
                         {
                             name: "Solidworks",
                             url: require("../assets/images/icons/solidworks.png").default
+                        },
+                        {
+                            name: "Docker",
+                            url: require("../assets/images/icons/docker.png").default
                         },
                         {
                             name: "Pytorch",
@@ -291,7 +295,7 @@ const showcase = {
                 {
                     name: "Teleoperating Robot through VR Interface using Velocity Controlled Treadmill",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: " ",
+                    summary: "This system encourages indoor exercise by entertaining the user using the VR interface. The user controls his/her 'Avatar' through the treadmill which adjusts the speed to the user automatically. The avatar can be in the virtual reality or an actual robot at the remote place",
                     paragraphs:["This is an ongoing research for my Master's Degree. The aim of research is to provide a system in which the user can access a robot located elsewhere in our world and perform tasks through the robot embodiment. Our objective is to provide a prototype for a system that is expected to be used widely in the near future.",
                     "The system requires a large variety of technologies to be integrated. Firstly, the passive control treadmill is made by augmenting an ordinary treadmill to receive velocity command from a single board computer, then we control the velocity of the treadmill based on the estimated walking pace of the user obtained by sensors. Combining the walking velocity with a handle attached by a force sensor, we now have both linear and angular velocity to be input to the robot.",
                     "Obviously, controlling a robot requires feedback, thus, the user wears the VR Head Mounted Display (HMD) to perceive the environment around the robot's vicinity. At the current stage, only visual feedback is provided, but we aim to implement the haptic feedback as well.",
@@ -333,7 +337,7 @@ const showcase = {
                 {
                     name: "Robot Arm Control Training",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: " ",
+                    summary: "The training course that every newcomers in my laboratory need to take. It provides basic knowledge about robot arm manipulator such as kinematics and implement the actual control software on a simulator and the actual robot.",
                     paragraphs:["My laboratory has a tradition of having the newcomers take a course on controlling an actual robot arm which gives the students a practical experience apart from courseworks. I took this training course and became the tutor of it in the following year for both spring and autumn semester.",
                     "The content of the training includes the theory of control like frame and coordination, kinematics, PID control, Polynomial interpolation, etc. The training requires a thorough grasp of theory and implementation on both simulation and a real machine",
                     "We moved from a robot arm simulation to an actual robot when I became the tutor, so I have to rewrite the source code of the training. However, to provide safety to the student, we didn’t use an actual industrial robot arm. Instead, we use a haptic device as a substitution."
@@ -359,13 +363,16 @@ const showcase = {
         },
         {
             name: "Mobile Application Developer",
-            background: "Started from an urge to use a technology to facilitate repeating daily life, I studied the mobile app development for this sole purpose at first.",
+            background: "From an urge to use a technology to facilitate repeating daily life, I started studying the mobile app development for this sole purpose. I currently have experience with only the Android.",
             projects:[
                 {
                     name: "Gym Buddy Application",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat con",
-                    paragraphs:[""
+                    summary: "My first Android application written in JAVA with the basic structures. It has various features designed for the gym-goers.",
+                    paragraphs:["This Android application is my own side project which is made for personal use and practice programming especially in application developing.",
+                    "This application is for memoing and scheduling your workout. You can create a routine of workouts and add a specific routine or workout to the schedule. Also, while doing workout, you can also use this app to memo down the weight and number of repetitions in each set easily.",
+                    "This project involves mostly basic JAVA language and a very simple application structure. For persisting user data, SQLite is used to save records locally.",
+                    "There are still a lot of features which are under development such as statistics to monitor the performance and records. UI is also unrefined due to this being my very first Android application."
                     ],
                     frameworks:[
                         {
@@ -383,8 +390,11 @@ const showcase = {
                 {
                     name: "Expense Tracker Application",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: " ",
-                    paragraphs:[""
+                    summary: "A one-day project I made to confirm my understanding of JAVA and android application. Even though the features are simple, but it was a great challenge to build a thing in very short time.",
+                    paragraphs:["This Android application is my own side project which is made for personal use and practice programming especially in application developing.",
+                    "Since 2020, I started to track my expenses each month, however, I could not find a desirable way to take down the note, hence I made this application out of my own need. This application is made as simple as it could be since that’s the main purpose of it.",
+                    "It has the simplest design to memo your expenses by just writing down the number and adding it. You can also change the category of the expense as well as adding the description. It also has the feature to automatically generate monthly expenses which repeat every month like housing or electricity.",
+                    "Note that the application looks very unrefined since it took merely 1 day from the design to the deployment (debugging excluded)"
                     ],
                     frameworks:[
                         {
@@ -396,18 +406,17 @@ const showcase = {
                         {
                             name: "Java",
                             url: require("../assets/images/icons/java.png").default
-                        },
-                        {
-                            name: "Kotlin",
-                            url: require("../assets/images/icons/kotlin.svg").default
                         }
                     ]
                 },
                 {
                     name: "Android Application Developer Internship @Teamlab",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: " ",
-                    paragraphs:[""
+                    summary: "My first internship which I work as an Android app developer. I experienced working in a team with professional code management and quality control.",
+                    paragraphs:["For an internship at Teamlab, I worked as an Android app developer to make an app renewal with small team. It was my first time working in the professional environment.",
+                    "Code management and quality control is done properly within the team and there are several meetings each week to make sure that Android team, IOS team, and UI UX designers are on the same page.",
+                    "This project uses structure of the Android architecture: separating files into modules such as UI, API, etc. It also utilize modern libraries which are great lessons for me. Despite being pretty tough for me at first (since I was totally a beginner back there), I really learned a lot from this internship.",
+                    "About the detail of the app, sorry that I cannot disclose the information."
                     ],
                     frameworks:[
                         {
@@ -423,6 +432,10 @@ const showcase = {
                         {
                             name: "Java",
                             url: require("../assets/images/icons/java.png").default
+                        },
+                        {
+                            name: "Kotlin",
+                            url: require("../assets/images/icons/kotlin.svg").default
                         }
                     ]
                 }
@@ -430,13 +443,13 @@ const showcase = {
         },
         {
             name: "Web Application Developer",
-            background: "",
+            background: "To broaden my knowledge and improve my abilities as developer, I started learning different tech stacks for web application.",
             projects:[
                 {
                     name: "Github Page Profile",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat con",
-                    paragraphs:[""
+                    summary: "My first webpage running on Nodejs, written by Reactjs, and published on Github. The content is basically random stuff about myself.",
+                    paragraphs:["By following tutorials and using a template, I made a Github page using Reactjs and CSS styling. This project is all about frontend."
                     ],
                     frameworks:[
                         {
@@ -466,8 +479,9 @@ const showcase = {
                 {
                     name: "Github Page Portfolio",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat con",
-                    paragraphs:[""
+                    summary: "An updated version of my first Github page which focuses on more professional portfolio. This time, I build the project from scratch and chooses components to put in thoroughly. The project is published using Github workflow.",
+                    paragraphs:["The second Github page which is made from scratch and has proper code structure, thus easier to maintain and add features in the future. The core of this frontend project is still the same with the previous one: Reactjs, Nodejs, and CSS. Additional to previous Github page, I utilise bootstrap, styled-components, etc. for styling.",
+                    "This page's purpose is to serve as a portfolio which is hopefully more interactive and attractive than the paper one."
                     ],
                     frameworks:[
                         {
@@ -477,10 +491,6 @@ const showcase = {
                         {
                             name: "Nodejs",
                             url: require("../assets/images/icons/nodejs.png").default
-                        },
-                        {
-                            name: "Docker",
-                            url: require("../assets/images/icons/docker.png").default
                         }
                     ],
                     languages:[
@@ -501,8 +511,11 @@ const showcase = {
                 {
                     name: "Student Portal Website",
                     image: require("../assets/images/showcases/dummy.png").default,
-                    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat con",
-                    paragraphs:[""
+                    summary: "An ongoing project with a friend for the purpose of studying new tech stack. Additional to the tech knowledge, we focus on communication and management as a team. We tried to build features that we want in a student portal site",
+                    paragraphs:["We find that our school's student portal site was hard to use at the point that we started the project. We tried to build the website with the desired features from scratch both frontend and backend.",
+                    "Currently, we are starting from courses search service, which has features to narrow down the result like search box, and several filters.",
+                    "For backend, we use Spring with Spring JPA/ Hibernate as an ORM (Object Relational Mapping) to communicate with MySQL database.",
+                    "For frontend, we use Reactjs with Redux to manage the states, and Axios to get courses from the backend and apply some filters on the frontend using state."
                     ],
                     frameworks:[
                         {
